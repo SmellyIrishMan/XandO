312d1e03dbfb7c586faf5b5a8ba322260a4c5a28
SmellyIrishMan <andy.fearon.alt@gmail.com>
2018-12-26 23:18:44 -0500

    Using a typedef.
    Experimented with switching the State over to an Option<State> of just  Player1 and Player2 but I don't think that's really the intended use of Option. There's always going to be a state. We just need to know which state it is.

--- XandO/Board.fs
+++ XandO/Board.fs
@@ -1,21 +1,21 @@
 ﻿module Board
 
 type State = AVAILABLE | PLAYER1 | PLAYER2
+type XandOBoard = State[,]
 
 let createEmptyBoard =
   Array2D.create<State> 3 3 State.AVAILABLE
 
-let markPosition (board : State[,]) row col (state : State) =
+let markPosition (board :XandOBoard) row col (state : State) =
   Array2D.init<State> 3 3 (fun r c -> if (r, c) = (row, col) then state
                                       else board.[r, c])
 
-
-let positionIsTaken (board : State[,]) row col =
+let positionIsTaken (board : XandOBoard) row col =
   let state = board.[row, col]
   match state with
-  | State.AVAILABLE -> false
   | State.PLAYER1 -> true
   | State.PLAYER2 -> true
+  | State.AVAILABLE -> false
 
 //let playerHasWon (board : State[,]) =
   
